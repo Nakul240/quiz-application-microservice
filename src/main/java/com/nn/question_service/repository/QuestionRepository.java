26cd30d4014c69e7c06cda744c4f8d0e9ad2ca45
Nakul240 <nakulnanjappa240@gmail.com>
2024-05-30 09:56:56 +0530

    findByCategory added in the repository

--- src/main/java/com/nn/question_service/repository/QuestionRepository.java
+++ src/main/java/com/nn/question_service/repository/QuestionRepository.java
@@ -3,5 +3,9 @@ package com.nn.question_service.repository;
 import com.nn.question_service.data.models.entity.Question;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends MongoRepository<Question,String> {
+
+    List<Question> findByCategory(String category);
 }
